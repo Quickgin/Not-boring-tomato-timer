4dfeaa328dedd770547d866a9b2d60d6839aff8d
Quickgin <WANL2_15@uni.worc.ac.uk>
2018-04-08 18:00:27 +0100

    qwe

---
+++ sample code/js/sensor.js
@@ -0,0 +1,57 @@
+var SHAKE_THRESHOLD = 4000;
+var watchID;
+var last = 0;
+var x, y, z, lastx = 0, lasty = 0, lastz = 0;
+
+$(document).on("pagecreate", "#page1", function () {
+	
+	//setup listener for the toggle switch
+	$("#flipswitch").on("change", function() {
+		
+		if( $(this).val() == "on" ) startSensor();
+		else if ( $(this).val() == "off" ) stopSensor();
+
+	});
+
+function startSensor() {
+	watchID = navigator.accelerometer.watchAcceleration( accelerometerSuccess, accelerometerError);
+}
+
+function stopSensor() {
+	navigator.accelerometer.clearWatch(watchID);
+
+}
+    
+function accelerometerSuccess(acceleration) {
+    var curTime = new Date().getTime();
+     if ((curTime-last_update)> 10) {
+          var diffTime = curTime -last_update;
+           last_update = curTime;
+           x = acceleration.x;
+           y = acceleration.y;
+           z = acceleration.z;
+           var speed = Math.abs(x +y + z - lastx - lasty - lastz) / diffTime * 10000;
+           if (speed > SHAKE_THRESHOLD) {
+               alert("hahaha");  // Do something
+           }
+           lastx = x;
+           lasty = y;
+           lastz = z;
+       }
+
+}
+
+function accelerometerError() {
+   alert('Error');
+}
+
+
+
+
+
+
+
+
+
+
+
