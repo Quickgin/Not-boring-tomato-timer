c5dd8b1e8ebbf3a18b56ffc92df0421b896d7d50
Letao Wang <wanl2_15@uni.worc.ac.uk>
2018-04-13 13:03:32 +0100

    test3

--- sample code/js/sensor.js
+++ sample code/js/sensor.js
@@ -3,27 +3,28 @@ var watchID;
 var last = 0;
 var x, y, z, lastx = 0, lasty = 0, lastz = 0;
 
-$(document).on("pagecreate", "#page1", function () {
+$(document).on("pagecreate", "#pageone", function () {
 	
 	//setup listener for the toggle switch
 	$("#flipswitch").on("change", function() {
 		
 		if( $(this).val() == "on" ) startSensor();
+        alert("a");
 		else if ( $(this).val() == "off" ) stopSensor();
-        alert("hjj");
+        
 	});
 });
 function startSensor() {
 	watchID = navigator.accelerometer.watchAcceleration( accelerometerSuccess, accelerometerError,{frequency:500});
-    alert("chou");
+    alert("b");
 }
 function stopSensor() {
 	navigator.accelerometer.clearWatch(watchID);
-     alert("meicuo");
+     alert("c");
 } 
 function accelerometerSuccess(acceleration) {
     var curTime = new Date().getTime();
-    alert("lala");
+    alert("d");
      if ((curTime-last)> 10) {
           var diffTime = curTime -last;
            last= curTime;
@@ -32,7 +33,7 @@ function accelerometerSuccess(acceleration) {
            z = acceleration.z;
            var speed = Math.abs(x +y + z - lastx - lasty - lastz) / diffTime * 10000;
            if (speed > SHAKE_THRESHOLD) {
-               alert("hahaha"); 
+               alert("e"); 
            }
            lastx = x;
            lasty = y;
