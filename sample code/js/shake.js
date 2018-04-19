8f211f74fe6652c137ada7004d2ae5ec179bfcfa
Quickgin <WANL2_15@uni.worc.ac.uk>
2018-04-19 22:41:59 +0100

    shake

---
+++ sample code/js/shake.js
@@ -0,0 +1,36 @@
+$(document).on("pagecreate", "#pageone", function () {
+
+    alert("pageone created");
+    
+	//setup listener for the toggle switch
+	$("#flipswitch").on("change", function(event) {
+		
+       if(this.value == "on" ) {
+            startSensor();
+           console.log("on");
+        }
+		else
+        {
+            stopSensor();
+            console.log("off");
+        }
+        
+
+	});
+});
+function startSensor() {
+shake.startWatch（onShake，40，onError);
+}
+function stopSensor() {
+shake.stopWatch（）;
+} 
+var onShake = function () {
+  // Fired when a shake is detected
+    alert("haha");
+};
+
+var onError = function () {
+  // Fired when there is an accelerometer error (optional)
+    
+    alert('Error');
+};
